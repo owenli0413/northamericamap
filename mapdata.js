28f1378dbd85d44247379995634a931fc9add44e
owenli0413 <lixiang0413@hotmail.com>
2024-05-07 23:01:35 -0600

    Add files via upload

---
+++ mapdata.js
@@ -0,0 +1,667 @@
+var simplemaps_namap_mapdata={
+  main_settings: {
+   //General settings
+    width: "600", //'700' or 'responsive'
+    background_color: "#FFFFFF",
+    background_transparent: "yes",
+    border_color: "#ffffff",
+    popups: "detect",
+    
+    //State defaults
+    state_description: "State description",
+    state_color: "#44a6c6",
+    state_hover_color: "#2c7891",
+    state_url: "",
+    border_size: 1.5,
+    all_states_inactive: "no",
+    all_states_zoomable: "no",
+    
+    //Location defaults
+    location_description: "Location description",
+    location_color: "#FF0067",
+    location_opacity: 0.8,
+    location_hover_opacity: 1,
+    location_url: "",
+    location_size: 25,
+    location_type: "square",
+    location_image_source: "frog.png",
+    location_border_color: "#FFFFFF",
+    location_border: 2,
+    location_hover_border: 2.5,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no",
+    
+    //Label defaults
+    label_color: "#d5ddec",
+    label_hover_color: "#d5ddec",
+    label_size: 14,
+    label_font: "Arial",
+    hide_labels: "no",
+    manual_zoom: "no",
+    back_image: "no",
+    arrow_color: "#cecece",
+    arrow_color_border: "#808080",
+    initial_back: "no",
+    initial_zoom: -1,
+    initial_zoom_solo: "no",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "yes",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+    
+    //Popup settings
+    popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "no",
+    
+    //Advanced settings
+    div: "map",
+    auto_load: "yes",
+    url_new_tab: "no",
+    images_directory: "default",
+    fade_time: 0.1,
+    import_labels: "no",
+    link_text: "View Website",
+    state_image_url: "",
+    state_image_position: "",
+    location_image_url: ""
+  },
+  state_specific: {
+    AB: {
+      name: "阿尔伯塔省 Alberta",
+      color: "#8bc076",
+      hover_color: "#62a04a"
+    },
+    AK: {
+      name: "阿拉斯加州 Alaska"
+    },
+    AL: {
+      name: "阿拉巴马州 Alabama"
+    },
+    AR: {
+      name: "阿肯色州 Arkansas"
+    },
+    AZ: {
+      name: "亚利桑那州 Arizona"
+    },
+    BC: {
+      name: "卑诗省 British Columbia",
+      color: "#8bc076",
+      hover_color: "#62a04a"
+    },
+    CA: {
+      name: "加利福尼亚州 California"
+    },
+    CO: {
+      name: "科罗拉多州 Colorado"
+    },
+    CT: {
+      name: "康涅狄格州 Connecticut"
+    },
+    DC: {
+      name: "哥伦比亚特区 District of Columbia"
+    },
+    DE: {
+      name: "特拉华州 Delaware"
+    },
+    FL: {
+      name: "佛罗里达州 Florida"
+    },
+    GA: {
+      name: "乔治亚州 Georgia"
+    },
+    HI: {
+      name: "夏威夷州 Hawaii"
+    },
+    IA: {
+      name: "爱荷华州 Iowa"
+    },
+    ID: {
+      name: "爱达荷州 Idaho"
+    },
+    IL: {
+      name: "伊利诺伊州 Illinois"
+    },
+    IN: {
+      name: "印第安纳州 Indiana"
+    },
+    KS: {
+      name: "堪萨斯州 Kansas"
+    },
+    KY: {
+      name: "肯塔基州\n Kentucky"
+    },
+    LA: {
+      name: "路易斯安那州 Louisiana"
+    },
+    MA: {
+      name: "马萨诸塞州 Massachusetts"
+    },
+    MB: {
+      name: "曼尼托巴省 Manitoba",
+      color: "#8bc076",
+      hover_color: "#62a04a"
+    },
+    MD: {
+      name: "马里兰州 Maryland"
+    },
+    ME: {
+      name: "缅因州 Maine"
+    },
+    MI: {
+      name: "密歇根州 Michigan"
+    },
+    MN: {
+      name: "明尼苏达州 Minnesota"
+    },
+    MO: {
+      name: "密苏里州 Missouri"
+    },
+    MS: {
+      name: "密西西比州 Mississippi"
+    },
+    MT: {
+      name: "蒙大拿州 Montana"
+    },
+    MX: {
+      name: "Mexico",
+      hide: "yes"
+    },
+    NB: {
+      name: "新不伦瑞克省 New Brunswick",
+      color: "#8bc076",
+      hover_color: "#62a04a"
+    },
+    NC: {
+      name: "北卡罗来纳州 North Carolina"
+    },
+    ND: {
+      name: "北达科他州 North Dakota"
+    },
+    NE: {
+      name: "内布拉斯加州 Nebraska"
+    },
+    NH: {
+      name: "新罕布什尔州 New Hampshire"
+    },
+    NJ: {
+      name: "新泽西州 New Jersey"
+    },
+    NL: {
+      name: "纽芬兰与拉布拉多省 Newfoundland and Labrador",
+      color: "#8bc076",
+      hover_color: "#62a04a"
+    },
+    NM: {
+      name: "新墨西哥州 New Mexico"
+    },
+    NS: {
+      name: "新斯科舍省 Nova Scotia",
+      color: "#8bc076",
+      hover_color: "#62a04a"
+    },
+    NT: {
+      name: "西北地区 Northwest Territories",
+      color: "#8bc076",
+      hover_color: "#62a04a"
+    },
+    NU: {
+      name: "努勒维特 Nunavut",
+      color: "#8bc076",
+      hover_color: "#62a04a"
+    },
+    NV: {
+      name: "内华达州 Nevada"
+    },
+    NY: {
+      name: "纽约州 New York"
+    },
+    OH: {
+      name: "俄亥俄州 Ohio"
+    },
+    OK: {
+      name: "俄克拉荷马州 Oklahoma"
+    },
+    ON: {
+      name: "安大略省 Ontario",
+      color: "#8bc076",
+      hover_color: "#62a04a"
+    },
+    OR: {
+      name: "俄勒冈州 Oregon"
+    },
+    PA: {
+      name: "宾夕法尼亚州 Pennsylvania"
+    },
+    PE: {
+      name: "爱德华王子岛省 Prince Edward Island",
+      color: "#8bc076",
+      hover_color: "#62a04a"
+    },
+    QC: {
+      name: "魁北克省 Québec",
+      color: "#8bc076",
+      hover_color: "#62a04a"
+    },
+    RI: {
+      name: "罗德岛州 Rhode Island"
+    },
+    SC: {
+      name: "南卡罗来纳州South Carolina"
+    },
+    SD: {
+      name: "南达科他州 South Dakota"
+    },
+    SK: {
+      name: "萨斯喀彻温省 Saskatchewan",
+      color: "#8bc076",
+      hover_color: "#62a04a"
+    },
+    TN: {
+      name: "田纳西州\n Tennessee"
+    },
+    TX: {
+      name: "德克萨斯州 Texas"
+    },
+    UT: {
+      name: "犹他州 Utah"
+    },
+    VA: {
+      name: "弗吉尼亚州 Virginia"
+    },
+    VT: {
+      name: "佛蒙特州 Vermont"
+    },
+    WA: {
+      name: "华盛顿州 Washington"
+    },
+    WI: {
+      name: "威斯康星州\n Wisconsin"
+    },
+    WV: {
+      name: "西弗吉尼亚州 West Virginia"
+    },
+    WY: {
+      name: "怀俄明州 Wyoming"
+    },
+    YT: {
+      name: "育空省 Yukon",
+      color: "#8bc076",
+      hover_color: "#62a04a"
+    }
+  },
+  locations: {},
+  labels: {
+    NH: {
+      parent_id: "NH",
+      display: "all",
+      pill: "yes",
+      width: 30,
+      x: 920,
+      y: 565
+    },
+    MA: {
+      parent_id: "MA",
+      display: "all",
+      pill: "yes",
+      width: 30,
+      x: 920,
+      y: 590
+    },
+    CT: {
+      parent_id: "CT",
+      display: "all",
+      pill: "yes",
+      width: 30,
+      x: 920,
+      y: 615
+    },
+    RI: {
+      parent_id: "RI",
+      display: "all",
+      pill: "yes",
+      width: 30,
+      x: 920,
+      y: 640
+    },
+    MD: {
+      parent_id: "MD",
+      display: "all",
+      pill: "yes",
+      width: 30,
+      x: 920,
+      y: 665
+    },
+    VT: {
+      parent_id: "VT",
+      display: "all",
+      pill: "yes",
+      width: 30,
+      x: 885,
+      y: 590
+    },
+    NJ: {
+      parent_id: "NJ",
+      display: "all",
+      pill: "yes",
+      width: 30,
+      x: 885,
+      y: 615
+    },
+    DE: {
+      parent_id: "DE",
+      display: "all",
+      pill: "yes",
+      width: 30,
+      x: 885,
+      y: 640
+    },
+    DC: {
+      parent_id: "DC",
+      display: "all",
+      pill: "yes",
+      width: 30,
+      x: 885,
+      y: 665
+    },
+    HI: {
+      parent_id: "HI",
+      display: "all",
+      pill: "yes",
+      width: 30,
+      x: 290,
+      y: 640
+    },
+    WV: {
+      parent_id: "WV",
+      x: "803.4",
+      y: "601.7",
+      size: 11
+    },
+    TX: {
+      parent_id: "TX",
+      x: 630,
+      y: 695
+    },
+    AK: {
+      parent_id: "AK",
+      x: 375,
+      y: 201
+    },
+    PE: {
+      parent_id: "PE",
+      display: "all",
+      pill: "yes",
+      width: 30,
+      x: 965,
+      y: 490
+    },
+    NS: {
+      parent_id: "NS",
+      display: "all",
+      pill: "yes",
+      width: 30,
+      x: 965,
+      y: 515
+    },
+    MX: {
+      parent_id: "MX",
+      x: "601.3",
+      y: "788.5",
+      name: "Mexico"
+    },
+    MB: {
+      parent_id: "MB",
+      x: "656.4",
+      y: "417.8"
+    },
+    SK: {
+      parent_id: "SK",
+      x: "595.6",
+      y: "425.6"
+    },
+    AB: {
+      parent_id: "AB",
+      x: "534.6",
+      y: "407"
+    },
+    BC: {
+      parent_id: "BC",
+      x: "470.8",
+      y: "383.5"
+    },
+    NU: {
+      parent_id: "NU",
+      x: "652",
+      y: "289.5"
+    },
+    NT: {
+      parent_id: "NT",
+      x: "528.4",
+      y: "293.2"
+    },
+    YT: {
+      parent_id: "YT",
+      x: "438.5",
+      y: "275.6"
+    },
+    ON: {
+      parent_id: "ON",
+      x: "729.8",
+      y: "452.6"
+    },
+    QC: {
+      parent_id: "QC",
+      x: "833.9",
+      y: "431.3"
+    },
+    NB: {
+      parent_id: "NB",
+      x: "900.6",
+      y: "483.3"
+    },
+    NL: {
+      parent_id: "NL",
+      x: "901.1",
+      y: "384.5"
+    },
+    MN: {
+      parent_id: "MN",
+      x: "677.5",
+      y: "520.6"
+    },
+    MT: {
+      parent_id: "MT",
+      x: "557.2",
+      y: "507.5"
+    },
+    ND: {
+      parent_id: "ND",
+      x: "630.3",
+      y: "509.3"
+    },
+    ID: {
+      parent_id: "ID",
+      x: "509.2",
+      y: "540.2"
+    },
+    WA: {
+      parent_id: "WA",
+      x: "476.8",
+      y: "485"
+    },
+    AZ: {
+      parent_id: "AZ",
+      x: "516.2",
+      y: "650.8"
+    },
+    CA: {
+      parent_id: "CA",
+      x: "447.7",
+      y: "608.3"
+    },
+    CO: {
+      parent_id: "CO",
+      x: "579.7",
+      y: "604.6"
+    },
+    NV: {
+      parent_id: "NV",
+      x: "481.5",
+      y: "582.4"
+    },
+    NM: {
+      parent_id: "NM",
+      x: "569.4",
+      y: "656.2"
+    },
+    OR: {
+      parent_id: "OR",
+      x: "462",
+      y: "524.9"
+    },
+    UT: {
+      parent_id: "UT",
+      x: "526.1",
+      y: "595.7"
+    },
+    WY: {
+      parent_id: "WY",
+      x: "567.6",
+      y: "556.2"
+    },
+    AR: {
+      parent_id: "AR",
+      x: "699.3",
+      y: "655.2"
+    },
+    IA: {
+      parent_id: "IA",
+      x: "687.5",
+      y: "573.4"
+    },
+    KS: {
+      parent_id: "KS",
+      x: "644.2",
+      y: "614.6"
+    },
+    MO: {
+      parent_id: "MO",
+      x: "697.5",
+      y: "617.3"
+    },
+    NE: {
+      parent_id: "NE",
+      x: "632.8",
+      y: "579.3"
+    },
+    OK: {
+      parent_id: "OK",
+      x: "656.4",
+      y: "650.3"
+    },
+    SD: {
+      parent_id: "SD",
+      x: "630.6",
+      y: "544.5"
+    },
+    LA: {
+      parent_id: "LA",
+      x: "699.3",
+      y: "691"
+    },
+    AL: {
+      parent_id: "AL",
+      x: "756.2",
+      y: "677.6"
+    },
+    FL: {
+      parent_id: "FL",
+      x: "813.1",
+      y: "719.2"
+    },
+    GA: {
+      parent_id: "GA",
+      x: "790.9",
+      y: "675.4"
+    },
+    MS: {
+      parent_id: "MS",
+      x: "727.7",
+      y: "679.7"
+    },
+    SC: {
+      parent_id: "SC",
+      x: "813.1",
+      y: "657"
+    },
+    IL: {
+      parent_id: "IL",
+      x: "724.8",
+      y: "593"
+    },
+    IN: {
+      parent_id: "IN",
+      x: "753.5",
+      y: "594.2"
+    },
+    KY: {
+      parent_id: "KY",
+      x: "772.2",
+      y: "617"
+    },
+    NC: {
+      parent_id: "NC",
+      x: "831.6",
+      y: "634.4"
+    },
+    OH: {
+      parent_id: "OH",
+      x: "781",
+      y: "587.7"
+    },
+    TN: {
+      parent_id: "TN",
+      x: "756.7",
+      y: "641.3"
+    },
+    VA: {
+      parent_id: "VA",
+      x: "822.2",
+      y: "611.8"
+    },
+    WI: {
+      parent_id: "WI",
+      x: "715.9",
+      y: "539.3"
+    },
+    NY: {
+      parent_id: "NY",
+      x: "844.7",
+      y: "541.3"
+    },
+    PA: {
+      parent_id: "PA",
+      x: "824.8",
+      y: "571.4"
+    },
+    ME: {
+      parent_id: "ME",
+      x: "882.7",
+      y: "502.6"
+    },
+    MI: {
+      parent_id: "MI",
+      x: "760.2",
+      y: "556.5"
+    }
+  },
+  legend: {
+    entries: []
+  },
+  regions: {}
+};
